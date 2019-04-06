8feeb3b3206d717a696b2563e7f5a1a66d7c6336
chrisuwimana1 <chrisuwimana1@gmail.com>
2019-04-06 10:28:19 -0500

    Lab 5

--- Lab05/lab5.js
+++ Lab05/lab5.js
@@ -1,11 +1,31 @@
 /* runs test to see if expected argument is === to value returned by function2test argument */
 function myFunctionTest(expected, function2test) {
-    if (expected === function2test()) {
-        return "TEST SUCCEEDED";
-    } else {
-        return "TEST FAILED.  Expected " + expected + " found " + function2test();
-    }
+    
+    if (Array.isArray(expected) && Array.isArray(function2test())){
 
+        
+        if (expected.length != function2test().length){
+
+            return "TEST FAILED.  Expected " + expected.toString() + " found " + function2test().toString();
+        }else{
+
+            for(var i = 0; i<expected.length; i++){
+                
+                if(expected[i] != function2test()[i]){
+                    return "TEST FAILED.  Expected " + expected.toString() + " found " + function2test().toString();
+                }
+            }
+            return "TEST SUCCEEDED"
+        }
+        
+    }else{
+        if (expected === function2test()) {
+            return "TEST SUCCEEDED";
+        } else {
+            return "TEST FAILED.  Expected " + expected + " found " + function2test();
+        }
+    }
+    
 }
 
 /* max returns the maximum of 2 arguments */
@@ -15,7 +35,7 @@ function max(a, b) {
     } else {
         return b;
     }
-
+    
 }
 console.log("Expected output of max(20,10) is 20  " + myFunctionTest(20, function () {
     return max(20, 10);
@@ -24,19 +44,19 @@ console.log("Expected output of max(20,10) is 20  " + myFunctionTest(20, functio
 /* max3 takes 3 numbers as arguments and returns the largest */
 function maxOfThree(a, b, c) {
     return max(max(a, b), c);
-
+    
 }
 
 console.log("Expected output of maxOfThree(5,4,44) is 44  " + myFunctionTest(44, function () {
     return maxOfThree(5, 4, 44);
 }));
 
-        // console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(55, function () {
-        //     return maxOfThree(55, 4, 44);
-        // }));
-        // console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(4, function () {
-        //     return maxOfThree(55, 4, 44);
-        // }));
+console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(55, function () {
+    return maxOfThree(55, 4, 44);
+}));
+console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(4, function () {
+    return maxOfThree(55, 4, 44);
+}));
 
 
 //Write a function isVowel() that takes a character (i.e. a string of length 1) and returns
@@ -101,7 +121,7 @@ console.log("Expected output of multiply([1,2,2,4]) is 20 " + myFunctionTest(20,
 //function reverse string
 function reverseString(inputString){
     var reversedString= "";
-
+    
     for (i=inputString.length -1; i>=0; i--){
         reversedString += inputString.charAt(i);
     }
@@ -118,11 +138,11 @@ console.log("Expected output of reverseString(\"aba\") is aba " + myFunctionTest
 
 //function to find the longest word
 function findLongestWord(inputWords){
-
+    
     var longestWord = inputWords[0];
     
     for(var i = 1; i<inputWords.length; i++){
-
+        
         if (inputWords[i].length >longestWord.length){
             longestWord = inputWords[i]
         }
@@ -139,20 +159,21 @@ console.log("Expected output of findLongestWord([\"mamama\",\"ma\",\"ner\",\"tri
 }));
 
 
-
 //function filter longer words
 function filterLongWords(inputWords, i){
-   var filteredArray= inputWords.filter(function(str){
+    var filteredArray= inputWords.filter(function(str){
         return str.length > i;
     })
+    
+    //console.log(typeof filteredArray)
     return filteredArray;
 }
-var expectedarr = ["mamama,trid"];
-console.log("Expected output of filterLongWords([\"mamama\",\"ma\",\"ner\",\"trid\"],3) is [mamama,trid]  " + myFunctionTest(expectedarr, function () {
+
+console.log("Expected output of filterLongWords([\"mamama\",\"ma\",\"ner\",\"trid\"],3) is [mamama,trid]  " + myFunctionTest(["mamama","trid"], function () {
     return filterLongWords(["mamama","ma","ner","trid"],3);
 }));
 
-console.log("Expected output of filterLongWords([\"mamama\",\"ma\",\"ner\",\"trid\"],3) is [ma,ner] " + myFunctionTest("ma,ner", function () {
+console.log("Expected output of filterLongWords([\"mamama\",\"ma\",\"ner\",\"trid\"],3) is [ma,ner] " + myFunctionTest(["ma","ner"], function () {
     return filterLongWords(["mamama","ma","ner","trid"],3);
 }));
 
@@ -160,26 +181,17 @@ console.log("Expected output of filterLongWords([\"mamama\",\"ma\",\"ner\",\"tri
 //Map
 const a = [1,3,5,3,3]; 
 const b = a.map(function(elem, i, array) {
-  return elem * 10;
+    return elem * 10;
 })
 console.log(b);
 
 //Filter
 const c = a.filter(function(elem, i, array){
-  return elem === 3;});
-console.log(c);
-
-//Reduce
-const d = a.reduce(function(prevValue, elem, i, array){
-  return prevValue * elem;
-});
-console.log(d);
-
-
-
-
-
-
-
-
-
+    return elem === 3;});
+    console.log(c);
+    
+    //Reduce
+    const d = a.reduce(function(prevValue, elem, i, array){
+        return prevValue * elem;
+    });
+    console.log(d);
