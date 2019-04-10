d0c064088b48b6fab907c84868f11a32cdaef612
chrisuwimana1 <chrisuwimana1@gmail.com>
2019-04-10 14:23:57 -0500

    Lab 7

--- Lab07/ascii.js
+++ Lab07/ascii.js
@@ -1,48 +1,97 @@
-function changeAnimation(){
-		let s=document.getElementById("animation");
-		if (s.value == "Exercise"){
-			document.getElementById("text-area").value = EXERCISE;
-		}else if (s.value == "Juggler"){
-			document.getElementById("text-area").value = JUGGLER;
-		}else if (s.value == "Bike"){
-			document.getElementById("text-area").value = BIKE;
-		}else if (s.value == "Dive"){
-			document.getElementById("text-area").value = DIVE;
-		}else {
-			document.getElementById("text-area").value = BLANK;
-		}
-}
 
-function changeSize(){
-		let s=document.getElementById("fontsize");
-		if (s.value == "Tiny"){
-			document.getElementById("text-area").style.fontSize = "7pt";
-		}else if (s.value == "Small"){
-			document.getElementById("text-area").style.fontSize = "10pt";	
-		}else if (s.value == "Large"){
-			document.getElementById("text-area").style.fontSize = "16pt";
-		}else if (s.value == "Extra Large"){
-			document.getElementById("text-area").style.fontSize = "24pt";
-		}else if(s.value == "XXL")
-			document.getElementById("text-area").style.fontSize = "32pt";
-		else {
-			document.getElementById("text-area").style.fontSize = "12pt";
-		}
-}
 
-function startAnimation(){
-
-	let s=document.getElementById("text-area");
-	if (s.value !== BLANK){
-		document.getElementById("myBtn").disabled = false;
-	}else{
-		console.log("BLANK")
+function changeSize() {
+	let s = document.getElementById("fontsize");
+	if (s.value == "Tiny") {
+		document.getElementById("text-area").style.fontSize = "7pt";
+	} else if (s.value == "Small") {
+		document.getElementById("text-area").style.fontSize = "10pt";
+	} else if (s.value == "Large") {
+		document.getElementById("text-area").style.fontSize = "16pt";
+	} else if (s.value == "Extra Large") {
+		document.getElementById("text-area").style.fontSize = "24pt";
+	} else if (s.value == "XXL")
+		document.getElementById("text-area").style.fontSize = "32pt";
+	else {
+		document.getElementById("text-area").style.fontSize = "12pt";
 	}
 }
 
-window.onload = function(){
+window.onload = function () {
 
-	document.getElementById("animation").onchange = changeAnimation;
-	document.getElementById("fontsize").onchange = changeSize;
-	document.getElementById("start").onclick = startAnimation;
+	var startButton = document.getElementById("start");
+	var stopButton = document.getElementById("stop");
+	var turbo = document.getElementById("turbo");
+	var animationDropDown = document.getElementById("animation");
+	var fontSizeDropDown = document.getElementById("fontsize");
+	var whichOne = document.getElementById("animation").value;
+	var textArea = document.getElementById("text-area");
+	var animate
+
+	var timeout = 250;
+	animationDropDown.onchange = changeAnimation;
+	fontSizeDropDown.onchange = changeSize;
+
+	startButton.onclick = startAnimation;
+	
+	turbo.onchange = function(){
+		console.log("yes");
+		if(this.checked==true){
+            timeout=50
+        }else{
+            timeout=250
+        }
+
+        clearInterval(animate)
+        startAnimation();
+	}
+
+	function changeAnimation() {
+		let s = document.getElementById("animation");
+		if (s.value == "Exercise") {
+			document.getElementById("text-area").value = EXERCISE;
+			whichOne = "EXERCISE";
+		} else if (s.value == "Juggler") {
+			document.getElementById("text-area").value = JUGGLER;
+			whichOne = "JUGGLER"
+		} else if (s.value == "Bike") {
+			document.getElementById("text-area").value = BIKE;
+			whichOne = "Bike"
+		} else if (s.value == "Dive") {
+			document.getElementById("text-area").value = DIVE;
+			whichOne = "Dive"
+		} else {
+			document.getElementById("text-area").value = BLANK;
+			whichOne = "Blank"
+		}
+	}
+
+	function startAnimation() {
+		console.log(whichOne);
+		var its = ANIMATIONS[whichOne].split("=====")
+		var i = 0;
+		animate = setInterval(function () {
+			textArea.className = fontSizeDropDown.value
+			textArea.value = its[i]
+			if (i == its.length - 1) {
+				i = 0
+			} else {
+				i += 1
+			}
+		}, timeout);
+
+		startButton.disabled = true
+		stopButton.disabled = false;
+		animationDropDown.disabled = true;
+		turbo.disabled = false;
+	}
+
+	stopButton.onclick = function () {
+		clearInterval(animate)
+		startButton.disabled = false
+		stopButton.disabled = true;
+		animationDropDown.disabled = false;
+		turbo.disabled = true;
+	}
+
 };
